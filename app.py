35edd733e17d7b3e7746f153f94b725e4deca483
1nisharg <80333086+1nisharg@users.noreply.github.com>
2024-07-25 15:17:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def main():
     Now you can convert your PDFs between international and regional languages, and vice versa, with just two clicks! Experience seamless language conversion and enhance your document accessibility today.
     """)
     
-    if st.button("Experience LangVault"):
+    if st.button("Experience LangVault", "https://pdf-translator--openrag.streamlit.app/"):
         st.write("Redirecting to LangVault...")
         st.experimental_rerun()
 
